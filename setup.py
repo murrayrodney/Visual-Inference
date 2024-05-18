6ccbb0f8994b4fe172daf909a0d96e4bbab009b5
Rodney Murray <murray.rodney.7@gmail.com>
2024-05-17 20:01:25 -0600

    Initial setup from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze data from a study of using visualizations for statistical inference.',
+    author='Rodney Murray',
+    license='MIT',
+)
